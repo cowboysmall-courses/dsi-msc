7bae71d142cea63238c5a08fac1aa705b68b9da9
Jerry Kiely <jerry@cowboysmall.com>
2024-08-20 16:24:13 +0100

    small change to script

--- scripts/script_01.py
+++ scripts/script_01.py
@@ -11,25 +11,43 @@ import yfinance as yf
 
 
 # %% 1 - retrieve data for DJIA
-INDICES = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]["Symbol"].to_list()
-
+INDICES    = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]["Symbol"].to_list()
+START_DATA = '2019-1-1'
+END_DATA   = '2023-12-31'
 
 
 # %% 2 - retrieve data for indices
-def retrieve_data(index, start_date = '2017-1-1', end_date = '2023-12-31', progress = False):
-    return yf.download(f'^{index}', start_date, end_date, progress = progress)
+data = yf.download(INDICES, START_DATA, END_DATA, progress = False)
+data.head()
 
 
 
 # %% 3 - 
-data = [retrieve_data(index) for index in INDICES]
+adjc = data["Adj Close"]
+adjc.head()
 
 
 
 # %% 4 - merge data with outer join
-merged = pd.concat(data, axis = 1)
+rows = adjc.shape[0]
+dropped = []
+for column in adjc.columns:
+    pc = adjc[column].isna().sum() / rows
+    if pc > 0:
+        dropped.append(column) 
 
 
 
-# %% 5 - impute missing data using LOCF (forward fill)
-merged.ffill(inplace = True)
+# %% 5 - 
+adjc = adjc.drop(dropped, axis = 1)
+adjc.head()
+
+
+# %% 6 - impute missing data using LOCF (forward fill)
+adjc = adjc.ffill()
+adjc.head()
+
+
+
+
+# %%
