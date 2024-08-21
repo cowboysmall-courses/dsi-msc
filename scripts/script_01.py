1b4cb387c4a1bca93b3a03b71f6cfbf0f557127c
Jerry Kiely <jerry@cowboysmall.com>
2024-08-21 22:26:41 +0100

    small change to script

--- scripts/script_01.py
+++ scripts/script_01.py
@@ -10,7 +10,6 @@ import statsmodels.api as sm
 
 import yfinance as yf
 
-from sklearn.preprocessing import scale
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
@@ -37,7 +36,6 @@ end_date   = "2023-12-31"
 
 # %% 2 - retrieve data for indices
 data = yf.download(indices, "2018-12-31", "2024-1-1", progress = False)
-data.head()
 
 
 
@@ -61,130 +59,94 @@ data_rets = pd.DataFrame({index: data_clse[index].pct_change(fill_method = None)
 # %% 3 - impute missing data using LOCF (forward fill)
 data_open = data_open.ffill()
 data_open = data_open[start_date:end_date]
-data_open.head()
 
 
 
 # %% 3 - impute missing data using LOCF (forward fill)
 data_clse = data_clse.ffill()
 data_clse = data_clse[start_date:end_date]
-data_clse.head()
 
 
 
 # %% 3 - impute missing data using LOCF (forward fill)
 data_rets = data_rets.ffill()
 data_rets = data_rets[start_date:end_date]
-data_rets.head()
 
 
 
 # %% 3 - 
-# data_rets_norm = (data_rets - data_rets.mean()) / data_rets.std()
-# data_rets_norm.head()
+def normed(data):
+    normed = (data - data.mean()) / data.std()
+    return normed.T 
 
-data_rets_norm = (data_rets - data_rets.min()) / (data_rets.max() - data_rets.min())
-data_rets_norm = data_rets_norm.T
-data_rets_norm.head()
-
-
-
-# %% 3 - 
-elbw = []
-sils = []
-
-K = list(range(2, 12))
-for i in  K:
-    cluster = KMeans(n_clusters = i).fit(data_rets_norm)
-    elbw.append(cluster.inertia_)
-    sils.append(silhouette_score(data_rets_norm.values, cluster.labels_))
+def scaled(data):
+    scaled = (data - data.min()) / (data.max() - data.min())
+    return scaled.T 
 
 
 
 # %% 3 - 
-fig, axes = plt.subplots(1, 2, figsize = (16, 6))
+def cluster_plots(data, title):
+    elbw = []
+    sils = []
 
-fig.suptitle("Cluster: Returns")
+    K = list(range(2, 12))
+    for i in  K:
+        cluster = KMeans(n_clusters = i).fit(data)
+        elbw.append(cluster.inertia_)
+        sils.append(silhouette_score(data.values, cluster.labels_))
 
-axes[0].plot(K, elbw, 'bx-')
-axes[0].set_title("Elbow Method")
-axes[0].set_xlabel("K")
-axes[0].set_ylabel("SSE")
+    fig, axes = plt.subplots(1, 2, figsize = (16, 6))
 
-axes[1].plot(K, sils, 'bx-')
-axes[1].set_title("Silhouette Method")
-axes[1].set_xlabel("K")
-axes[1].set_ylabel("Silhouette Score")
+    fig.suptitle(f"Cluster: {title}")
 
-plt.show()
+    axes[0].plot(K, elbw, 'bx-')
+    axes[0].set_title("Elbow Method")
+    axes[0].set_xlabel("K")
+    axes[0].set_ylabel("SSE")
 
+    axes[1].plot(K, sils, 'bx-')
+    axes[1].set_title("Silhouette Method")
+    axes[1].set_xlabel("K")
+    axes[1].set_ylabel("Score")
 
-
-# %% 3 - 
-data_clst = pd.DataFrame(KMeans(n_clusters = 5).fit_predict(data_rets_norm), columns = ["Cluster"], index = data_rets_norm.index)
-data_clst.head()
-
-
-
-# %% 3 - 
-sizes = np.bincount(data_clst["Cluster"])
-print(sizes)
-
-
-
-
-
+    plt.show()
 
 
 
 # %% 3 - 
-data_clse_norm = (data_clse - data_clse.min()) / (data_clse.max() - data_clse.min())
-data_clse_norm = data_clse_norm.T
-data_clse_norm.head()
+data_rets_normed = normed(data_rets)
+cluster_plots(data_rets_normed, "Returns - Normed")
+
+data_rets_normed_cls = pd.DataFrame(KMeans(n_clusters = 4).fit_predict(data_rets_normed), columns = ["Cluster"], index = data_rets_normed.index)
+print(np.bincount(data_rets_normed_cls["Cluster"]))
 
 
 
 # %% 3 - 
-elbw = []
-sils = []
+data_rets_scaled = scaled(data_rets)
+cluster_plots(data_rets_scaled, "Returns - Scaled")
 
-K = list(range(2, 12))
-for i in  K:
-    cluster = KMeans(n_clusters = i).fit(data_clse_norm)
-    elbw.append(cluster.inertia_)
-    sils.append(silhouette_score(data_clse_norm.values, cluster.labels_))
+data_rets_scaled_cls = pd.DataFrame(KMeans(n_clusters = 4).fit_predict(data_rets_scaled), columns = ["Cluster"], index = data_rets_scaled.index)
+print(np.bincount(data_rets_scaled_cls["Cluster"]))
 
 
 
 # %% 3 - 
-fig, axes = plt.subplots(1, 2, figsize = (16, 6))
+data_clse_normed = normed(data_clse)
+cluster_plots(data_clse_normed, "Close - Normed")
 
-fig.suptitle("Cluster: Close")
-
-axes[0].plot(K, elbw, 'bx-')
-axes[0].set_title("Elbow Method")
-axes[0].set_xlabel("K")
-axes[0].set_ylabel("SSE")
-
-axes[1].plot(K, sils, 'bx-')
-axes[1].set_title("Silhouette Method")
-axes[1].set_xlabel("K")
-axes[1].set_ylabel("Silhouette Score")
-
-plt.show()
+data_clse_normed_cls = pd.DataFrame(KMeans(n_clusters = 4).fit_predict(data_clse_normed), columns = ["Cluster"], index = data_clse_normed.index)
+print(np.bincount(data_clse_normed_cls["Cluster"]))
 
 
 
 # %% 3 - 
-data_clst = pd.DataFrame(KMeans(n_clusters = 5).fit_predict(data_rets_norm), columns = ["Cluster"], index = data_rets_norm.index)
-data_clst.head()
+data_clse_scaled = scaled(data_clse)
+cluster_plots(data_clse_scaled, "Close - Scaled")
 
-
-
-# %% 3 - 
-sizes = np.bincount(data_clst["Cluster"])
-print(sizes)
-
+data_clse_scaled_cls = pd.DataFrame(KMeans(n_clusters = 4).fit_predict(data_clse_scaled), columns = ["Cluster"], index = data_clse_scaled.index)
+print(np.bincount(data_clse_scaled_cls["Cluster"]))
 
 
 
@@ -200,4 +162,4 @@ print(sizes)
 
 
 
-# %%
+# %% 7 - 
