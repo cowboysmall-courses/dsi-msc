08ad7c646e70f75e47f2e5bdecce39aedaae514c
Jerry Kiely <jerry@cowboysmall.com>
2024-08-30 19:28:58 +0100

    added script - DBSCAN clustering + moved cluster scripts into kmeans module

---
+++ scripts/cluster/dbscan/script_01.py
@@ -0,0 +1,110 @@
+
+
+# %% 0 - import required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+
+import statsmodels.api as sm
+
+import yfinance as yf
+
+from itertools import combinations
+
+from statsmodels.tsa.stattools import coint
+
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
+
+import warnings
+warnings.filterwarnings("ignore", category = FutureWarning)
+
+
+
+# %% 1 - retrieve data for DJIA
+indices    = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]["Symbol"].to_list()
+start_date = "2018-01-01"
+end_date   = "2023-12-31"
+
+
+
+# %% 2 - retrieve data for indices
+data = yf.download(indices, "2017-12-31", "2024-01-01", progress = False)
+
+
+
+# %% 2 - 
+adjc = data["Close"].dropna(axis = 1)
+rets = adjc.pct_change().iloc[1:, :]
+print(rets.shape)
+
+
+
+# %% 3 - 
+rets["AAPL"].plot(figsize = (16, 6))
+
+
+
+# %% 4 - 
+# rets = rets.T
+
+
+# %% 5 - 
+pca = PCA(n_components = 50)
+# comp = pca.fit_transform(rets.T)
+pca.fit(rets)
+comp = pca.components_
+
+
+
+
+# %% 5 - 
+print(comp.shape)
+
+
+# %% 5 - 
+X = comp.T
+print(X)
+
+
+# %% 5 - 
+X = preprocessing.StandardScaler().fit_transform(X)
+
+
+# %% 5 - 
+clst = DBSCAN(eps = 3)
+# clst = DBSCAN()
+clst.fit(X)
+
+
+# %% 5 - 
+lbls = clst.labels_
+print(np.bincount(lbls + 1))
+
+
+
+
+# %% 5 - 
+tsne = TSNE().fit_transform(X)
+
+
+
+# %% 5 - 
+plt.figure(figsize = (16, 9))
+plt.title("Clusters")
+
+plt.scatter(tsne[(lbls != -1), 0], tsne[(lbls != -1), 1], s = 100, alpha = 0.85, c = lbls[lbls != -1], cmap = cm.Paired)
+plt.scatter(tsne[(lbls == -1), 0], tsne[(lbls == -1), 1], s = 100, alpha = 0.05)
+
+plt.axis("off")
+plt.show()
+
+
+
+
+
+# %%
