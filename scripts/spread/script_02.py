3f61f59151e5ece8720fa2128220b0ef92e1852d
Jerry Kiely <jerry@cowboysmall.com>
2024-09-03 16:36:02 +0100

    added scripts on regression + added some papers

--- scripts/spread/script_02.py
+++ scripts/spread/script_02.py
@@ -20,16 +20,19 @@ msft = yf.download("MSFT", "2017-12-31", "2024-01-01", progress = False)
 
 
 # %% 2 - 
-aapl["Relative Close"] = aapl["Close"] / aapl["Close"][0]
-msft["Relative Close"] = msft["Close"] / msft["Close"][0]
+aapl["Returns"] = aapl["Close"].pct_change()
+msft["Returns"] = msft["Close"].pct_change()
+
+aapl = aapl[1:]
+msft = msft[1:]
 
 
 
 # %% 4 - 
-X = np.log(msft["Relative Close"])
+X = np.log(1 + msft["Returns"])
 X = sm.add_constant(X)
 
-Y = np.log(aapl["Relative Close"])
+Y = np.log(1 + aapl["Returns"])
 
 
 
@@ -43,49 +46,19 @@ print(result.summary())
 alpha = result.params.values[0]
 beta  = result.params.values[1]
 
-resi  = Y - (alpha + (beta * X["Relative Close"]))
+resi  = Y - (alpha + (beta * X["Returns"]))
 
 
 
 # %% 4 - 
 plt.figure(figsize = (16, 9))
 
-plt.title("Spread")
+plt.title("Spread of Log Returns")
 plt.xlabel("Time")
 plt.ylabel("Spread")
 
 plt.plot(resi)
 
-plt.show()
-
-
-
-# %% 3 - 
-plt.figure(figsize = (16, 9))
-
-plt.title("Close")
-plt.xlabel("Time")
-plt.ylabel("Close")
-
-plt.plot(aapl["Close"], label = "AAPL")
-plt.plot(msft["Close"], label = "MSFT")
-
-plt.legend()
-plt.show()
-
-
-
-# %% 4 - 
-plt.figure(figsize = (16, 9))
-
-plt.title("Relative Close")
-plt.xlabel("Time")
-plt.ylabel("Close")
-
-plt.plot(aapl["Relative Close"], label = "AAPL")
-plt.plot(msft["Relative Close"], label = "MSFT")
-
-plt.legend()
 plt.show()
 
 
